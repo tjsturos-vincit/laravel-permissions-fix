d7e73d6226847b4cd6801dbb11712e79bedc7732
Tyler Sturos <86961455+tjsturos-vincit@users.noreply.github.com>
2023-06-06 09:45:37 -0800

    Update perm_fix.sh

--- perm_fix.sh
+++ perm_fix.sh
@@ -3,22 +3,22 @@ echo "Starting"
 # BEGIN Fix Laravel Permissions Script
 
 # Adding owner to web server group
-sudo usermod -a -G ${LARAVEL_WS_GROUP} ${LARAVEL_OWNER}
+usermod -a -G ${LARAVEL_WS_GROUP} ${LARAVEL_OWNER}
 
 # Set files owner/group
-sudo chown -R ${LARAVEL_OWNER}:${LARAVEL_WS_GROUP} ${LARAVEL_ROOT}
+chown -R ${LARAVEL_OWNER}:${LARAVEL_WS_GROUP} ${LARAVEL_ROOT}
 
 # Set correct permissions for directories 
-sudo find ${LARAVEL_ROOT} -type f -exec chmod 644 {} \;
+find ${LARAVEL_ROOT} -type f -exec chmod 644 {} \;
 
 # Set correct permissions for files 
-sudo find ${LARAVEL_ROOT} -type d -exec chmod 755 {} \;
+find ${LARAVEL_ROOT} -type d -exec chmod 755 {} \;
 
 # Set webserver group for storage + cache folders
-sudo chgrp -R ${LARAVEL_WS_GROUP} ${LARAVEL_ROOT}/storage ${LARAVEL_ROOT}/bootstrap/cache
+chgrp -R ${LARAVEL_WS_GROUP} ${LARAVEL_ROOT}/storage ${LARAVEL_ROOT}/bootstrap/cache
 
 # Set correct permissions for storage + cache folders
-sudo chmod -R ug+rwx ${LARAVEL_ROOT}/storage ${LARAVEL_ROOT}/bootstrap/cache
+chmod -R ug+rwx ${LARAVEL_ROOT}/storage ${LARAVEL_ROOT}/bootstrap/cache
 
 # END Fix Laravel Permissions Script
 echo "Finished"
